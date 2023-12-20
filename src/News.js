dc4743d3ebeed465ffd01743d65904465f4dce45
Montso Moalosi <montsoernestmoalosi@gmail.com>
2023-12-20 20:34:39 +0200

    Impliment if statement if url value is null for the images

--- src/News.js
+++ src/News.js
@@ -2,7 +2,11 @@ export default function News(props) {
   return (
     <div className="news">
         <div className="news-img">
-            <img src={props.article.urlToImage}/>
+            {
+                props.article.urlToImage!==null?
+                <img src={props.article.urlToImage}/>:
+                <img src="https://www.freeiconspng.com/thumbs/no-image-icon/no-image-icon-15.png"/>
+            }
         </div>
         <h1>{props.article.title}</h1>
         <p>{props.article.description?.substring(0,100).concat("...")}<a href={props.article.url} target="_target">See more</a></p>
