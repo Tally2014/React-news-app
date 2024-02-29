ea863795adca166db2b7da1992cffbe885d32548
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-03-01 01:30:14 +0200

    Update the url Api to date so the app can run

--- src/App.js
+++ src/App.js
@@ -1,57 +1,54 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import News from './News';
+import { useEffect, useState } from "react";
+import "./App.css";
+import News from "./News";
 
 function App() {
+  let [articles, setArticles] = useState([]);
+  let [category, setCategory] = useState("gaming");
 
-  let[articles,setArticles] = useState([]);
-  let[category,setCategory] = useState("gaming");
-
-  useEffect(()=>{
-
-    var url = 'https://newsapi.org/v2/everything?' +
-          `q=${category}&` +
-          'from=2023-12-18&' +
-          'apiKey=8e25809a096149d5ba4d480b6623f1b6';
+  useEffect(() => {
+    var url =
+      "https://newsapi.org/v2/everything?" +
+      `q=${category}&` +
+      "from=2024-2-18&" +
+      "apiKey=8e25809a096149d5ba4d480b6623f1b6";
     var req = new Request(url);
 
     fetch(req)
-    .then((response)=> response.json())
-    .then((news)=>{
-      console.log(news.articles);
-      setArticles(news.articles);
-    }).catch((err)=>{
-      console.log(err);
-    })
-
-  },[category])
+      .then((response) => response.json())
+      .then((news) => {
+        console.log(news.articles);
+        setArticles(news.articles);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [category]);
   return (
     <div className="App">
-      <header className='header'>
+      <header className="header">
         <h1>News 101</h1>
-        <input type="text" placeholder='Search News' onChange={(event)=>{
-          if(event.target.value !==""){
-            setCategory(event.target.value);
-          }
-          else{
-            setCategory("top-trending")
-          }
-          
-        }}></input>
+        <input
+          type="text"
+          placeholder="Search News"
+          onChange={(event) => {
+            if (event.target.value !== "") {
+              setCategory(event.target.value);
+            } else {
+              setCategory("top-trending");
+            }
+          }}
+        ></input>
       </header>
-      <section className='news-articles'>
-        {
-          articles.length!==0?
-          articles.map((article)=>{
-            return(
-              <News article = {article}/>
-            )
+      <section className="news-articles">
+        {articles.length !== 0 ? (
+          articles.map((article) => {
+            return <News article={article} />;
           })
-          :
+        ) : (
           <h3>No News Found For Searched Text</h3>
-        }
+        )}
       </section>
-      
     </div>
   );
 }
--- src/News.js
+++ src/News.js
@@ -2,11 +2,22 @@ export default function News(props) {
   return (
     <div className="news">
         <div className="news-img">
-            {
-                props.article.urlToImage!==null?
-                <img src={props.article.urlToImage}/>:
-                <img src="https://www.freeiconspng.com/thumbs/no-image-icon/no-image-icon-15.png"/>
-            }
+        {props.article.urlToImage !== null && (
+  <img
+    src={props.article.urlToImage}
+    onLoad={(e) => {
+      if (e.target.naturalWidth >= 20 && e.target.naturalHeight >= 20) {
+        e.target.style.width = '100%'; // Optional: Set the width to 100% if it's larger
+      } else {
+        e.target.src = 'https://www.freeiconspng.com/thumbs/no-image-icon/no-image-icon-15.png';
+      }
+    }}
+    onError={(e) => {
+      e.target.src = 'https://www.freeiconspng.com/thumbs/no-image-icon/no-image-icon-15.png';
+    }}
+    alt="Article Image"
+  />
+)}
         </div>
         <h1>{props.article.title}</h1>
         <p>{props.article.description?.substring(0,100).concat("...")}<a href={props.article.url} target="_target">See more</a></p>
